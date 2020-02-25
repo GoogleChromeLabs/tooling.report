2b4bcf273085db4a8c8970a4169ccef58250a081
Jason Miller <jason@developit.ca>
2020-02-25 15:34:17 -0500

    attempt to use details_url

--- .github/actions/preview/index.js
+++ .github/actions/preview/index.js
@@ -1,5 +1,5 @@
 import { exec } from '@actions/exec';
-import { getInput, startGroup, endGroup, setFailed } from '@actions/core';
+import { getInput, startGroup, endGroup, setFailed, setOutput } from '@actions/core';
 import { GitHub, context } from '@actions/github';
 
 async function run(github, context) {
@@ -28,22 +28,70 @@ async function run(github, context) {
     endGroup();
     
     startGroup(`Deploying to Firebase`);
-    const json = await exec(firebase, [
+    let buf = [];
+    const deploymentExitCode = await exec(firebase, [
         'hosting:preview',
         '--token',
         firebaseToken,
         '--json'
-    ]);
+    ], {
+        listeners: {
+            stdout(data) {
+                buf.push(data);
+            }
+        }
+    });
+    const deploymentText = Buffer.concat(buf).toString('utf-8');
+    const deployment = JSON.parse(deploymentText);
     endGroup();
 
-    console.log(json);
+    if (deployment.status !== 'success') {
+        throw Error(deploymentText);
+    }
+
+    let url = deployment.result.previews.default.url;
+
+    if (getInput('use-web-tld')) {
+        url = url.replace(/\.firebaseapp\.com$/, '.web.app');
+    }
+
+    setOutput('details_url', url);
+
+    // return { url };
+
+    /*
+    {
+        "status": "success",
+        "result": {
+            "previews": {
+                "default": {
+                    "target": null,
+                    "site": "tooling-report",
+                    "url": "https://tooling-report--18d5620fef5fe2d9.firebaseapp.com",
+                    "name": "sites/tooling-report/versions/18d5620fef5fe2d9"
+                }
+            }
+        }
+    }
+    */
 }
 
 (async () => {
-	try {
-		const token = getInput('repo-token');
-		const github = token ? new GitHub(token) : {};
-		await run(github, context);
+    const token = process.env.GITHUB_TOKEN || getInput('repo-token');
+    const github = token ? new GitHub(token) : {};
+    // if (github.context) {
+    //     const {owner, repo} = github.context.repo;
+    //     const check = await github.checks.create({
+    //         owner,
+    //         repo,
+    //         name: CHECK_NAME,
+    //         head_sha: github.context.sha,
+    //         status: 'in_progress',
+    //     });
+    //     // const checkId = check.data.id;
+    // }
+    try {
+        await run(github, context);
 	} catch (e) {
 		setFailed(e.message);
 	}
