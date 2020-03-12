80c780717313986e0299cd7d3cb63861611cbade
Jake Archibald <jaffathecake@gmail.com>
2020-03-12 15:36:28 +0000

    Rollup: Fixing HTML entry test

--- tests/non-js-resources/subtests/entry/rollup/lib/html-plugin.js
+++ tests/non-js-resources/subtests/entry/rollup/lib/html-plugin.js
@@ -17,75 +17,71 @@ import postHTML from 'posthtml';
 import postHTMLURLS from 'posthtml-urls';
 
 const prefix = 'html:';
-const deleteMe = 'DELETE_ME';
 const assetRe = new RegExp('/fake/path/to/asset/([^/]+)/', 'g');
 
 export default function() {
   let emittedHTMLIds;
+  let htmlEntries;
+
+  async function loadHTML(id, rollupContext) {
+    const parsedPath = parsePath(id);
+    rollupContext.addWatchFile(id);
+    const file = await fsp.readFile(id);
+
+    const htmlResult = await postHTML()
+      .use(
+        postHTMLURLS({
+          eachURL: async url => {
+            const match = /^(chunk|asset):(.+)/.exec(url);
+            if (!match) return;
+            const [type, relativePath] = match.slice(1);
+            const fullPath = resolvePath(dirname(id), relativePath);
+            const fileId = await (async () => {
+              if (type === 'chunk') {
+                return rollupContext.emitFile({
+                  type: 'chunk',
+                  id: fullPath,
+                });
+              }
+              const source = await fsp.readFile(fullPath);
+              const parsedPath = parsePath(relativePath);
+
+              return rollupContext.emitFile({
+                type: 'asset',
+                name: parsedPath.base,
+                source,
+              });
+            })();
+
+            return `/fake/path/to/asset/${fileId}/`;
+          },
+        }),
+      )
+      .process(file);
+
+    const fileId = rollupContext.emitFile({
+      type: 'asset',
+      source: htmlResult.html,
+      fileName: parsedPath.base,
+    });
+
+    emittedHTMLIds.push(fileId);
+  }
 
   return {
     name: 'html',
-    buildStart() {
+    options(opts) {
+      const inputs = Array.isArray(opts.input) ? opts.input : [opts.input];
+      htmlEntries = inputs.filter(id => id.startsWith(prefix));
+      opts.input = inputs.filter(id => !id.startsWith(prefix));
       emittedHTMLIds = [];
     },
-    async resolveId(id, importer) {
-      if (!id.startsWith(prefix)) return null;
-
-      const realId = id.slice(prefix.length);
-      const resolveResult = await this.resolve(realId, importer);
-      if (!resolveResult) {
-        throw Error(`Cannot resolve ${realId} from ${importer}`);
-      }
-      return prefix + resolveResult.id;
-    },
-    async load(id) {
-      if (!id.startsWith(prefix)) return;
-
-      const realId = id.slice(prefix.length);
-      const parsedPath = parsePath(realId);
-      this.addWatchFile(realId);
-      const file = await fsp.readFile(realId);
-
-      const htmlResult = await postHTML()
-        .use(
-          postHTMLURLS({
-            eachURL: async url => {
-              const match = /^(chunk|asset):(.+)/.exec(url);
-              if (!match) return;
-              const [type, relativePath] = match.slice(1);
-              const fullPath = resolvePath(dirname(realId), relativePath);
-              const fileId = await (async () => {
-                if (type === 'chunk') {
-                  return this.emitFile({
-                    type: 'chunk',
-                    id: fullPath,
-                  });
-                }
-                const source = await fsp.readFile(fullPath);
-                const parsedPath = parsePath(relativePath);
-
-                return this.emitFile({
-                  type: 'asset',
-                  name: parsedPath.base,
-                  source,
-                });
-              })();
-
-              return `/fake/path/to/asset/${fileId}/`;
-            },
-          }),
-        )
-        .process(file);
-
-      const fileId = this.emitFile({
-        type: 'asset',
-        source: htmlResult.html,
-        fileName: parsedPath.base,
-      });
-
-      emittedHTMLIds.push(fileId);
-
-      return deleteMe;
+    async buildStart() {
+      await Promise.all(
+        htmlEntries.map(htmlEntry =>
+          loadHTML(htmlEntry.slice(prefix.length), this),
+        ),
+      );
     },
     async generateBundle(options, bundle) {
       const htmlAssets = emittedHTMLIds.map(id => this.getFileName(id));
@@ -97,12 +93,6 @@ export default function() {
             return '/' + this.getFileName(p1);
           },
         );
-      }
-
-      for (const [key, item] of Object.entries(bundle)) {
-        if (item.code && item.code.startsWith(deleteMe)) {
-          delete bundle[key];
-        }
       }
     },
   };
