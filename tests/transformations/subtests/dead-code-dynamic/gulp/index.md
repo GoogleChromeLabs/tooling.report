f9edf8f842be7edc6511cf4403fc896ba2006da7
Mariko Kosaka <mariko.kosaka@gmail.com>
2020-03-16 08:05:02 -0400

    dead code dynamic test

---
+++ tests/transformations/subtests/dead-code-dynamic/gulp/index.md
@@ -0,0 +1,5 @@
+---
+result: fail
+---
+
+`tinyify` does not eliminate dead code from dynamically imported module.
---
+++ tests/transformations/subtests/dead-code-dynamic/gulp/src/index.js
@@ -0,0 +1,4 @@
+(async function() {
+  const { logCaps } = await require('./utils/utils.js');
+  logCaps('This is index');
+})();
