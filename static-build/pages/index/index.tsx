1ef0856248fc4f51fc758dceef09d4550360a941
Adam Argyle <atom@argyleink.com>
2020-03-26 08:07:09 -0700

    sticky <ToolNav> (#74)
    * extract out to a component and hook up to data and images
    
    * removing commented out code

--- static-build/pages/index/index.tsx
+++ static-build/pages/index/index.tsx
@@ -19,7 +19,6 @@ import sharedStyles from 'css-bundle:static-build/shared/styles/index.css';
 import pageStyles from 'css-bundle:./styles.css';
 import {
   $topSticky,
-  $comparedTools,
   $sidebarLayout,
   $header,
   $summaryList,
@@ -28,8 +27,10 @@ import bundleURL, { imports } from 'client-bundle:client/home/index.ts';
 import { calculateScore, calculateScoreTotals } from 'static-build/utils';
 import Logo from '../../components/Logo/index';
 import Footer from '../../components/Footer/index';
-import LinkList from '../../components/LinkList/index.js';
+import LinkList from '../../components/LinkList/index';
 import SummaryCard from '../../components/SummaryCard/index';
+import ToolNav from '../../components/ToolNav/index';
+
 import gulp from 'asset-url:../../img/gulp.svg';
 import rollup from 'asset-url:../../img/rollup.svg';
 import webpack from 'asset-url:../../img/webpack.svg';
@@ -140,63 +141,25 @@ const IndexPage: FunctionalComponent<Props> = ({ tests }: Props) => {
         </header>
         <main>
           <section id="getting-started">
-            <h3>Getting Started</h3>
-            <p>
-              Expand or reduce the scope of your comparison using the below
-              checklist. You can always come back and reconfigure it later.{' '}
-            </p>
-            <div>
-              <p>Use the (+) button to submit or request a build tool!</p>
-            </div>
-          </section>
-
-          <section id="configure">
             <div class={$sidebarLayout}>
-              <aside>
-                <h3>Configure</h3>
-                <small>Which build tools do you want to compare?</small>
-                <div>
-                  x/<b>5</b>
-                </div>
-              </aside>
+              <aside></aside>
               <div>
-                <ul>
-                  <li>
-                    <label for="webpackv4">Webpack v4</label>
-                    <input type="checkbox" name="webpackv4" id="webpackv4" />
-                  </li>
-                  <li>
-                    <label for="webpackv4">Webpack v4</label>
-                    <input type="checkbox" name="webpackv4" id="webpackv4" />
-                  </li>
-                  <li>
-                    <label for="webpackv4">Webpack v4</label>
-                    <input type="checkbox" name="webpackv4" id="webpackv4" />
-                  </li>
-                </ul>
+                <h3>Getting Started</h3>
+                <p>
+                  Expand or reduce the scope of your comparison using the below
+                  checklist. You can always come back and reconfigure it later.{' '}
+                </p>
+                <div>
+                  <p>Use the (+) button to submit or request a build tool!</p>
+                </div>
               </div>
             </div>
           </section>
 
-          <div className={`${$comparedTools} ${$topSticky}`}>
-            <figure>
-              <img />
-              <figcaption>Webpack v4</figcaption>
-            </figure>
-            <figure>
-              <img />
-              <figcaption>Webpack v4</figcaption>
-            </figure>
-            <figure>
-              <img />
-              <figcaption>Webpack v4</figcaption>
-            </figure>
-          </div>
-
           <section id="summary">
             <div class={$sidebarLayout}>
               <aside>
-                <h3 class={$topSticky}>Summary</h3>
+                <h3>Summary</h3>
                 <small>Results at the highest level</small>
               </aside>
               <div>{renderSummary(tests)}</div>
@@ -206,7 +169,7 @@ const IndexPage: FunctionalComponent<Props> = ({ tests }: Props) => {
           <section id="overview">
             <div class={$sidebarLayout}>
               <aside>
-                <h3 class={$topSticky}>Overview</h3>
+                <h3>Overview</h3>
                 <ul>
                   <li>section 1</li>
                   <li>section 2</li>
@@ -216,6 +179,8 @@ const IndexPage: FunctionalComponent<Props> = ({ tests }: Props) => {
               <div>{renderSummary(tests)}</div>
             </div>
           </section>
+
+          <ToolNav />
 
           <section id="configuration">
             <div class={$sidebarLayout}>
