e55393b92f81846ebd6a61c15f4700866bdce8b4
Adam Argyle <atom@argyleink.com>
2020-03-27 11:35:53 -0700

    Icon / SVG components (#63)
    * home icon
    
    * remove debug color
    
    * brand report duotone icon
    
    * lamp asset
    
    * heroes g2g
    
    * organized and added remaining icons
    
    * footer plant
    
    * new github fab component using new icon
    
    * footer uses new asset
    
    * header and footer use new assets
    
    * update to una's color name
    
    * fixes merge resolution cruft
    
    * collection and detail feature different heroes
    
    * add and use shadow variables
    
    * precision pass size reduction
    
    * minified and made svg files for most assets

--- static-build/components/Footer/index.tsx
+++ static-build/components/Footer/index.tsx
@@ -1,7 +1,8 @@
 import { h } from 'preact';
-import Logo from '../../components/Logo/index';
-import LinkList from '../../components/LinkList/index';
-import { $container, $footer, $rule, $desc } from './styles.css';
+import Logo from '../../components/Logo';
+import LinkList from '../../components/LinkList';
+import Plant from '../../components/Plant';
+import { $container, $footer, $rule, $desc, $messaging } from './styles.css';
 
 const links = [
   { title: 'Privacy Policy', href: '#' },
@@ -16,14 +17,21 @@ function Footer() {
   return (
     <footer class={$footer}>
       <div class={$container}>
-        <Logo />
-        <p class={$desc}>
-          Leverage agile frameworks to provide a robust synopsis for high level
-          overviews. Iterative approaches to corporate strategy foster
-          collaborative thinking to further the overall value proposition.
-          Organically grow the holistic world view of disruptive innovation via
-          workplace diversity and empowerment.
-        </p>
+        <div class={$messaging}>
+          <div>
+            <Logo />
+            <p class={$desc}>
+              Leverage agile frameworks to provide a robust synopsis for high
+              level overviews. Iterative approaches to corporate strategy foster
+              collaborative thinking to further the overall value proposition.
+              Organically grow the holistic world view of disruptive innovation
+              via workplace diversity and empowerment.
+            </p>
+          </div>
+          <div>
+            <Plant />
+          </div>
+        </div>
         <hr class={$rule} />
         <LinkList links={links} />
       </div>
