c08e549e0614642a03579a23ea7d6bb3de530f11
Adam Argyle <atom@argyleink.com>
2020-04-03 14:26:40 -0700

    working client side routing with selects

--- static-build/components/Breadcrumbs/client.ts
+++ static-build/components/Breadcrumbs/client.ts
@@ -1,8 +1,25 @@
 const navs = document.querySelectorAll('#breadcrumbs select');
+const urlPath = window.location.pathname.split('/');
+
+urlPath.pop();
+urlPath.shift();
 
 navs.forEach(nav => {
   nav?.addEventListener('input', e => {
     const target = <HTMLInputElement>e.target;
-    console.log(target?.value);
+    const data = target.dataset;
+    var index: number = -1;
+
+    if (data) {
+      index = parseInt(data.depth as string);
+    }
+
+    if (index !== -1) {
+      urlPath[index] = target?.value.slice(0, -1);
+
+      window.location.pathname = urlPath
+        .slice(0, index + 1)
+        .reduce((paths, path) => `${paths}/${path}`, '');
+    }
   });
 });
