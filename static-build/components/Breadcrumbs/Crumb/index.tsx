140ba668ecf7c36dff40d729af9d83e6b085e063
Adam Argyle <atom@argyleink.com>
2020-04-06 13:58:31 -0700

    broke <Crumb> out

---
+++ static-build/components/Breadcrumbs/Crumb/index.tsx
@@ -0,0 +1,53 @@
+import { h, FunctionalComponent, Fragment } from 'preact';
+import testData from 'test-data:';
+import { testURLMap } from '../../../shared/scripts/testurl';
+import { parentMap } from '../../../shared/scripts/parentmap';
+import { $divider, $collection, $iconbutton } from './styles.css';
+
+interface CrumbProps {
+  test: Test;
+  index: number;
+  current: Test;
+}
+
+const Crumb: FunctionalComponent<CrumbProps> = ({ test, index, current }) => {
+  const parentTest = parentMap.get(test);
+  const siblingTests = parentTest?.subTests || testData;
+
+  return (
+    <Fragment>
+      <span class={$divider}>//</span>
+      <span class={$collection}>
+        {test === current ? (
+          <span>{test.meta.title}</span>
+        ) : (
+          <a href={testURLMap.get(test)}>{test.meta.title}</a>
+        )}
+        <span class={$iconbutton}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 9 5"
+            width="16px"
+            height="6px"
+          >
+            <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
+          </svg>
+          <select data-depth={index}>
+            {siblingTests &&
+              Object.entries(siblingTests).map(([path, t]) =>
+                t === test ? (
+                  <option selected value={path + '/'}>
+                    {t.meta.title}
+                  </option>
+                ) : (
+                  <option value={path + '/'}>{t.meta.title}</option>
+                ),
+              )}
+          </select>
+        </span>
+      </span>
+    </Fragment>
+  );
+};
+
+export default Crumb;
--- static-build/components/Breadcrumbs/index.tsx
+++ static-build/components/Breadcrumbs/index.tsx
@@ -1,61 +1,9 @@
-import { h, FunctionalComponent, JSX, Fragment } from 'preact';
-import testData from 'test-data:';
+import { h, FunctionalComponent } from 'preact';
 import { parentMap } from '../../shared/scripts/parentmap';
-import { testURLMap } from '../../shared/scripts/testurl';
 import { HomeIcon } from '../Icons/';
-import {
-  $breadcrumbs,
-  $home,
-  $divider,
-  $collection,
-  $iconbutton,
-} from './styles.css';
-
-interface CrumbProps {
-  test: Test;
-  index: number;
-  current: Test;
-}
-
-const Crumb: FunctionalComponent<CrumbProps> = ({ test, index, current }) => {
-  const parentTest = parentMap.get(test);
-  const siblingTests = parentTest?.subTests || testData;
-
-  return (
-    <Fragment>
-      <span class={$divider}>//</span>
-      <span class={$collection}>
-        {test === current ? (
-          <span>{test.meta.title}</span>
-        ) : (
-          <a href={testURLMap.get(test)}>{test.meta.title}</a>
-        )}
-        <span class={$iconbutton}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 9 5"
-            width="16px"
-            height="6px"
-          >
-            <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
-          </svg>
-          <select data-depth={index}>
-            {siblingTests &&
-              Object.entries(siblingTests).map(([path, t]) =>
-                t === test ? (
-                  <option selected value={path + '/'}>
-                    {t.meta.title}
-                  </option>
-                ) : (
-                  <option value={path + '/'}>{t.meta.title}</option>
-                ),
-              )}
-          </select>
-        </span>
-      </span>
-    </Fragment>
-  );
-};
+import { $breadcrumbs, $home } from './styles.css';
+import Crumb from './Crumb';
+import { $collection, $iconbutton } from './Crumb/styles.css';
 
 interface BreadcrumbProps {
   test: Test;
