30207d953b11690f3350f46615815d101f982eeb
Adam Argyle <atom@argyleink.com>
2020-04-07 12:31:31 -0700

    early return and some code cleanup

--- client/test/breadcrumbs.ts
+++ client/test/breadcrumbs.ts
@@ -7,15 +7,14 @@ urlPaths.shift();
 navs.forEach(nav => {
   nav.addEventListener('input', event => {
     const target = <HTMLInputElement>event.target;
-    const data = target.dataset;
-    let index = data.depth ? parseInt(data.depth!) : -1;
+    const index = target.dataset?.depth ? parseInt(target.dataset.depth!) : -1;
 
-    if (index !== -1) {
-      urlPaths[index] = target?.value.slice(0, -1);
+    if (index === -1) return;
 
-      window.location.pathname = urlPaths
-        .slice(0, index + 1)
-        .reduce((urls, url) => `${urls}/${url}`, '');
-    }
+    urlPaths[index] = target?.value.slice(0, -1);
+
+    window.location.pathname = urlPaths
+      .slice(0, index + 1)
+      .reduce((urls, url) => `${urls}/${url}`, '');
   });
 });
