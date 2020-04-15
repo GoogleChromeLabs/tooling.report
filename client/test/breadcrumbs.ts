19a79ffceb0dbd1a0ae3dd900a90e627ab34c618
Adam Argyle <atom@argyleink.com>
2020-04-15 09:17:56 -0700

    fixes #134

--- client/test/breadcrumbs.ts
+++ client/test/breadcrumbs.ts
@@ -5,6 +5,19 @@ const urlPaths = window.location.pathname.split('/');
 urlPaths.pop();
 urlPaths.shift();
 
+const state = {
+  prevent: false,
+};
+
+const keyTab = 9;
+const keyEnter = 13;
+const keySpace = 32;
+const keyUpArrow = 38;
+const keyDownArrow = 40;
+
+const allowedKeys = [keyTab, keyEnter, keySpace];
+const preventedKeys = [keyUpArrow, keyDownArrow];
+
 window.onload = () => {
   breadcrumbs?.lastElementChild?.scrollIntoView({
     block: 'nearest',
@@ -13,14 +26,24 @@ window.onload = () => {
 };
 
 navs.forEach(nav => {
-  nav.addEventListener('input', event => {
+  nav.addEventListener('change', event => {
     const target = <HTMLInputElement>event.target;
     const index = target.dataset?.depth ? parseInt(target.dataset.depth!) : -1;
 
-    if (index === -1) return;
+    if (index === -1 || state.prevent) return;
 
     urlPaths[index] = target.value.slice(0, -1);
 
     window.location.pathname = urlPaths.slice(0, index + 1).join('/');
+  });
+
+  nav.addEventListener('keydown', event => {
+    const { keyCode } = event as KeyboardEvent;
+
+    if (preventedKeys.includes(keyCode)) {
+      state.prevent = true;
+    } else if (allowedKeys.includes(keyCode)) {
+      state.prevent = false;
+    }
   });
 });
