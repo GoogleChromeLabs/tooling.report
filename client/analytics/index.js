679103f2d98ecfcbfec08c499c995401539c4108
Jake Archibald <jaffathecake@gmail.com>
2020-04-29 10:17:50 +0100

    Some code golf of analytics

--- client/analytics/index.js
+++ client/analytics/index.js
@@ -11,87 +11,86 @@
  * limitations under the License.
  */
 
-// Taken from https://minimalanalytics.com/
-var a = window,
-  b = 'UA-164226855-1',
-  c = {
-    anonymizeIp: true,
-    colorDepth: true,
-    characterSet: true,
-    screenSize: true,
-    language: true,
-  },
-  d = a.history,
-  e = document,
-  f = navigator || {},
-  g = localStorage,
-  h = encodeURIComponent,
-  i = d.pushState,
-  k = function() {
-    return Math.random().toString(36);
-  },
-  l = function() {
-    return g.cid || (g.cid = k()), g.cid;
-  },
-  m = function(r) {
-    var s = [];
-    for (var t in r)
-      r.hasOwnProperty(t) && void 0 !== r[t] && s.push(h(t) + '=' + h(r[t]));
-    return s.join('&');
-  },
-  n = function(r, s, t, u, v, w, x) {
-    var z = 'https://www.google-analytics.com/collect',
-      A = m({
-        v: '1',
-        ds: 'web',
-        aip: c.anonymizeIp ? 1 : void 0,
-        tid: b,
-        cid: l(),
-        t: r || 'pageview',
-        sd:
-          c.colorDepth && screen.colorDepth
-            ? screen.colorDepth + '-bits'
-            : void 0,
-        dr: e.referrer || void 0,
-        dt: e.title,
-        dl: e.location.origin + e.location.pathname + e.location.search,
-        ul: c.language ? (f.language || '').toLowerCase() : void 0,
-        de: c.characterSet ? e.characterSet : void 0,
-        sr: c.screenSize
-          ? (a.screen || {}).width + 'x' + (a.screen || {}).height
-          : void 0,
-        vp:
-          c.screenSize && a.visualViewport
-            ? (a.visualViewport || {}).width +
-              'x' +
-              (a.visualViewport || {}).height
-            : void 0,
-        ec: s || void 0,
-        ea: t || void 0,
-        el: u || void 0,
-        ev: v || void 0,
-        exd: w || void 0,
-        exf: 'undefined' != typeof x && !1 == !!x ? 0 : void 0,
-      });
-    if (f.sendBeacon) f.sendBeacon(z, A);
-    else {
-      var y = new XMLHttpRequest();
-      y.open('POST', z, !0), y.send(A);
-    }
-  };
-(d.pushState = function(r) {
-  return (
-    'function' == typeof d.onpushstate && d.onpushstate({ state: r }),
-    setTimeout(n, c.delay || 10),
-    i.apply(d, arguments)
+// Based on https://minimalanalytics.com/
+const trackingId = 'UA-164226855-1';
+const options = {
+  anonymizeIp: true,
+  colorDepth: true,
+  characterSet: true,
+  screenSize: true,
+  language: true,
+};
+const getId = () => {
+  if (!localStorage.cid) {
+    localStorage.cid = Math.random().toString(36);
+  }
+  return localStorage.cid;
+};
+const track = (
+  type,
+  eventCategory,
+  eventAction,
+  eventLabel,
+  eventValue,
+  exceptionDescription,
+  exceptionFatal,
+) => {
+  navigator.sendBeacon(
+    'https://www.google-analytics.com/collect',
+    new URLSearchParams({
+      v: '1',
+      ds: 'web',
+      aip: options.anonymizeIp ? 1 : undefined,
+      tid: trackingId,
+      cid: getId(),
+      t: type || 'pageview',
+      sd:
+        options.colorDepth && screen.colorDepth
+          ? `${screen.colorDepth}-bits`
+          : undefined,
+      dr: document.referrer || undefined,
+      dt: document.title,
+      dl:
+        document.location.origin +
+        document.location.pathname +
+        document.location.search,
+      ul: options.language
+        ? (navigator.language || '').toLowerCase()
+        : undefined,
+      de: options.characterSet ? document.characterSet : undefined,
+      sr: options.screenSize
+        ? `${(self.screen || {}).width}x${(self.screen || {}).height}`
+        : undefined,
+      vp:
+        options.screenSize && self.visualViewport
+          ? `${(self.visualViewport || {}).width}x${
+              (self.visualViewport || {}).height
+            }`
+          : undefined,
+      ec: eventCategory || undefined,
+      ea: eventAction || undefined,
+      el: eventLabel || undefined,
+      ev: eventValue || undefined,
+      exd: exceptionDescription || undefined,
+      exf:
+        typeof exceptionFatal !== 'undefined' && !!exceptionFatal === false
+          ? 0
+          : undefined,
+    }),
   );
-}),
-  n(),
-  (a.ma = {
-    trackEvent: function o(r, s, t, u) {
-      return n('event', r, s, t, u);
-    },
-    trackException: function q(r, s) {
-      return n('exception', null, null, null, null, r, s);
-    },
-  });
+};
+const trackEvent = (category, action, label, value) =>
+  track('event', category, action, label, value);
+const trackException = (description, fatal) =>
+  track('exception', null, null, null, null, description, fatal);
+const originalPushState = history.pushState;
+history.pushState = function(state) {
+  history.onpushstate({ state });
+  setTimeout(track, options.delay || 10);
+  return originalPushState.apply(history, arguments);
+};
+track();
+self.ma = {
+  trackEvent,
+  trackException,
+};
