6805cf4d63cc7cd516fe9868a4077debcf2150f3
Adam Argyle <argyle@google.com>
2020-05-12 11:17:00 -0700

    add site description to site config data

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 export const testSubjects = ['rollup', 'webpack', 'parcel', 'gulp'];
 export const githubRepository =
   'https://github.com/GoogleChromeLabs/tooling.report/';
+export const metaDescription =
+  'a quick and easy way to figure out what the best tool for your next project is, if it’s worth your time to migrate from one tool to another and how to adopt a best practice into your existing code base';
