fdeddad679e0b7fcc697bef591aa60b947be8535
Adam Argyle <argyle@google.com>
2020-05-12 11:47:27 -0700

    fixes #153

---
+++ static-build/components/HeadMeta/social.tsx
@@ -0,0 +1,19 @@
+import { h, Fragment } from 'preact';
+import toolingReportSocial from 'asset-url:../../img/tooling-report-social.png';
+
+export default function SocialMeta() {
+  return (
+    <Fragment>
+      <meta property="og:type" content="article" />
+      <meta property="og:image" content={toolingReportSocial} />
+      <meta property="og:url" content="https://tooling.report" />
+      <meta
+        property="og:site_name"
+        content="tooling.report | brought to you by web.dev"
+      />
+      <meta name="twitter:image" content={toolingReportSocial} />
+      <meta name="twitter:site" content="@ChromiumDev" />
+      <meta name="twitter:creator" content="@ChromiumDev" />
+    </Fragment>
+  );
+}
