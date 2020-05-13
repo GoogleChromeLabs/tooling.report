709b962dbb851dae5530e40dfa00c3316fd5141f
Adam Argyle <argyle@google.com>
2020-05-13 08:32:39 -0700

    adds webdev to description

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const testSubjects = ['rollup', 'webpack', 'parcel', 'gulp'];
 export const githubRepository =
   'https://github.com/GoogleChromeLabs/tooling.report/';
 export const metaDescription =
-  'a quick and easy way to figure out what the best tool for your next project is, if it’s worth your time to migrate from one tool to another and how to adopt a best practice into your existing code base';
+  'A quick and easy way to figure out what the best tool for your next project is, if it’s worth your time to migrate from one tool to another and how to adopt a best practice into your existing code base. Brought to you by web.dev';
