d283321c44f9066724188c825ebd00fc38ca4b1e
Adam Argyle <argyle@google.com>
2020-05-13 08:48:07 -0700

    added alt image text to twitter

--- static-build/components/HeadMeta/social.tsx
+++ static-build/components/HeadMeta/social.tsx
@@ -8,9 +8,13 @@ export default function SocialMeta() {
       <meta property="og:image" content={toolingReportSocial} />
       <meta property="og:url" content="https://tooling.report" />
       <meta property="og:site_name" content="tooling.report" />
-      <meta name="twitter:image" content={toolingReportSocial} />
       <meta name="twitter:site" content="@ChromiumDev" />
       <meta name="twitter:creator" content="@ChromiumDev" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image:alt"
+        content="tooling.report logo at top followed by the homepage summary score cards, which offer a high level glimpse into the results."
+      />
     </Fragment>
   );
 }
