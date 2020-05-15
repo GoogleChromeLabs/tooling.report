88d760eac7c14fec01423ef191672c1937d754ba
Surma <surma@surma.dev>
2020-05-15 11:58:47 +0100

    Update config.js
    Co-authored-by: Jake Archibald <jaffathecake@gmail.com>

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ export const githubRepository =
 export const metaDescription =
   'A quick and easy way to figure out what the best tool for your next project is, if it’s worth your time to migrate from one tool to another and how to adopt a best practice into your existing code base. Brought to you by web.dev';
 
-const build = new Date();
-export const buildDate = `${build.getDate()}/${build.getMonth() +
-  1}/${build.getFullYear()}`;
+const now = new Date();
+export const buildDate = [
+  now.getUTCFullYear(),
+  (now.getUTCMonth() + 1).toString().padStart(2, '0'),
+  now.getUTCDate().toString().padStart(2, '0'),
+].join('/');
