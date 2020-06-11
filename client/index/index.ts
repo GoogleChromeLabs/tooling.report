4b536411bb1d021dd83dab7ce28074b77a2d1c37
Jake Archibald <jaffathecake@gmail.com>
2020-06-11 10:47:26 +0100

    Reminder for later

--- client/index/index.ts
+++ client/index/index.ts
@@ -10,4 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// NOTE: This script is loaded async on the index page.
 import './tooltips';
