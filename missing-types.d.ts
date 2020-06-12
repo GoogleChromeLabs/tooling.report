4a64d4c34c1c534113781e0a0c16a748cdad7b99
Jake Archibald <jaffathecake@gmail.com>
2020-06-12 12:09:56 +0100

    Better defs for custom elements

--- missing-types.d.ts
+++ missing-types.d.ts
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/// <reference path="./web-components.d.ts" />
 
 declare module 'asset-url:*' {
   const value: string;
@@ -87,4 +86,21 @@ declare module 'consts:config' {
     metaDescription: string;
   };
   export default value;
+}
+
+declare module 'preact/src/jsx' {
+  namespace JSXInternal {
+    interface IntrinsicElements {
+      'grid-tooltip': GridTooltipAttributes;
+    }
+    interface HTMLAttributes {}
+    interface GridTooltipAttributes extends HTMLAttributes {
+      content: string;
+      result: string;
+      testname: string;
+      tool: BuildTool;
+      href: string;
+      category?: string;
+    }
+  }
 }
