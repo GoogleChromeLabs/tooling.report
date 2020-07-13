435ca2d1f25fa2cc73fd042f89a70add5e3334f1
Ajay Poshak <poshakajay@gmail.com>
2020-07-13 18:08:35 +0530

    Setting minSize to zero is enough to trigger the splitting of chunks for every import

--- tests/code-splitting/subtests/dynamic-import-preloading/webpack/webpack.config.js
+++ tests/code-splitting/subtests/dynamic-import-preloading/webpack/webpack.config.js
@@ -7,10 +7,7 @@ module.exports = {
      * These are not to be used in production grade code.
      */
     splitChunks: {
-      chunks: 'all',
-      minChunks: 1,
-      minSize: 10,
-      maxSize: 10,
+      minSize: 0,
     },
   },
   plugins: [new HTMLWebpackPlugin()],
