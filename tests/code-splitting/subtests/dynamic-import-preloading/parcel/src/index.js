4abb29579d68e8256aece129b4348ccbfb38c840
Ajay Poshak <poshakajay@gmail.com>
2020-08-19 19:38:17 +0530

    Update the test code to add more randomness

--- tests/code-splitting/subtests/dynamic-import-preloading/parcel/src/index.js
+++ tests/code-splitting/subtests/dynamic-import-preloading/parcel/src/index.js
@@ -1,6 +1,6 @@
-(() => {
-  import('./utilsA.js');
-  if (Math.random() < 0.5) {
-    import('./utilsB.js');
-  }
-})();
+import('./utilsA.js').then(importedModule =>
+  importedModule.shout('this is index'),
+);
+if (Math.random() < 0.5) {
+  import('./utilsB.js');
+}
