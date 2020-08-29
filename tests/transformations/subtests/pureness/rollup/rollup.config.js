f830f863a15135fee714652734c034437ec1e3c7
Ajay Poshak <poshakajay@gmail.com>
2020-08-29 18:38:27 +0530

    Add pureness test for rollup

---
+++ tests/transformations/subtests/pureness/rollup/rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: './src/index.js',
+  output: {
+    dir: 'build',
+    format: 'esm',
+  },
+};
---
+++ tests/transformations/subtests/pureness/rollup/src/lib.js
@@ -0,0 +1,7 @@
+function createDatabaseConnection() {
+  console.log('connecting!');
+  return {};
+}
+
+export const foo = /*#__PURE__*/ createDatabaseConnection();
+export const bar = 'bar';
