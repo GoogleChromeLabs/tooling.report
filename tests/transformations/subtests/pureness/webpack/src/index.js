e722dfc7cf2e5645d3fe60d559cc7187ed61769d
Ajay Poshak <poshakajay@gmail.com>
2020-08-29 18:38:07 +0530

    Add pureness test for webpack

---
+++ tests/transformations/subtests/pureness/webpack/src/index.js
@@ -0,0 +1,2 @@
+import { bar } from './lib.js';
+console.log(bar);
---
+++ tests/transformations/subtests/pureness/webpack/webpack.config.js
@@ -0,0 +1,5 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'production',
+  plugins: [new HTMLWebpackPlugin()],
+};
